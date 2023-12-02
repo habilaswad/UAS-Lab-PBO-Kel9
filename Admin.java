bed1d832dab77f2e226be7dcfca21e4718bf4a05
Firjatullah Afny Abus <124426546+firjafny@users.noreply.github.com>
2023-12-02 22:19:11 +0700

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,15 @@
+/**
+ * Kelas Admin mewakili admin yang memegang kendali terhadap data dalam sistem.
+ * Kelas ini merupakan turunan dari kelas Akun.
+ *
+ * @author Firjatullah Afny Abus
+ * @version 1.0
+ */
+public class Admin extends Akun {
+  /**
+   * konstruktor default untuk kelas Admin.
+   */
+  public Admin() {
+    super();
+  }
+}
